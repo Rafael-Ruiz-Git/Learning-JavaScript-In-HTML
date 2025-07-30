8d70ce4021464519f7f25193a658d396770d722a
Rafael Ruiz <ruiz2099alex@gmail.com>
2025-07-30 12:00:54 -0400

    Update commit message

--- Objects/Intro-Arrays.js
+++ Objects/Intro-Arrays.js
@@ -12,3 +12,16 @@ console.log(mixedArray[4]["key"]); // alex
 console.log(mixedArray[4].key); // alex
 
 console.log(mixedArray[5][1]); // 2
+
+let numbersArray = new Array(6, 7, 8, 9, 10); // Creating an array using the Array constructor
+console.log(numbersArray); 
+
+let newFFruits = [...fruits, "kiwi", "pear"]; // Using spread operator to create a new array
+console.log(newFFruits); // ["apple", "banana", "Orange", "Mango
+
+
+fruits.push("grape"); // Adding an element to the end of the array
+console.log(fruits); // ["apple", "banana", "Orange", "Mango", "grape"]
+
+fruits.pop(); // Removing the last element from the array
+console.log(fruits); // ["apple", "banana", "Orange", "Mango"]
